32c08342560ad971e6cd50ca01781e027ce10d9c
GuzmanAriel <guzmanariel525@gmail.com>
2025-03-31 18:34:39 -0500

    Favorites

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { COMMENTS } from '../shared/comments';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleFavorite } from '../features/favorites/favoritesSlice';
 import RenderCampsite from '../features/campsites/RenderCampsite';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
-    const [comments, setComments] = useState(COMMENTS);
-    const [favorite, setFavorite] = useState(false); // ✅ New state
+
+    const comments = useSelector((state) => state.comments);
+    const favorites = useSelector((state) => state.favorites);
+    const dispatch = useDispatch();
 
     const renderCommentItem = ({ item }) => {
         return (
@@ -20,7 +23,7 @@ const CampsiteInfoScreen = ({ route }) => {
 
     return (
         <FlatList
-            data={comments.filter(
+            data={comments.commentsArray.filter(
                 (comment) => comment.campsiteId === campsite.id
             )}
             renderItem={renderCommentItem}
@@ -28,10 +31,10 @@ const CampsiteInfoScreen = ({ route }) => {
             contentContainerStyle={{ marginHorizontal: 20, paddingVertical: 20 }}
             ListHeaderComponent={
                 <>
-                    <RenderCampsite 
-                        campsite={campsite} 
-                        isFavorite={favorite}              
-                        markFavorite={() => setFavorite(true)} 
+                    <RenderCampsite
+                        campsite={campsite}
+                        isFavorite={favorites.includes(campsite.id)}
+                        markFavorite={() => dispatch(toggleFavorite(campsite.id))}
                     />
                     <Text style={styles.commentsTitle}>Comments</Text>
                 </>
